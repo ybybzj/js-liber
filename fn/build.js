7943f92d7bda624d5e372f876e60ee1cd6770677
jackzj <ybybzj@gmail.com>
2017-02-04 10:21:57 +0800

    update fn/build

--- fn/build.js
+++ fn/build.js
@@ -1,6 +1,7 @@
 var assert = require('../assert');
 var type = require('../type');
 var clone = require('../l/clone');
+var slice = require('../slice');
 //fn only accept one object as its parameter
 //paramsDescription's format:
 // {
@@ -9,7 +10,7 @@ var clone = require('../l/clone');
 //    '?pName': checker | [checker, defaultValue], //optional param
 //    ...
 // }
-function makeBuilder(fn, paramsDescription, fnName) {
+function makeBuilder(fn, paramsDescription, options) {
   assert(type.isFunction(fn),
     '[makeBuilder]first argument should be a function! given ' + fn,
     TypeError);
@@ -17,31 +18,46 @@ function makeBuilder(fn, paramsDescription, fnName) {
     '[makeBuilder]second argument should be an object! given ' + paramsDescription,
     TypeError);
 
+  var buildFnName = 'build';
+  var cloneFnName = 'clone';
 
-  var builderProto = {
-    build: function() {
-      var params = buildParams(this._paramsForBuild, paramsDescription, fnName);
-      return fn(params);
-    },
-    clone: function() {
-      var builderProto = this._builderProto;
-      var builder = Object.create(builderProto);
-      builder._paramsForBuild = clone(this._paramsForBuild);
-      builder._builderProto = builderProto;
-      return builder;
-    }
+  options = options || {};
 
+  if (type.isString(options)) {
+    options = {
+      fnName: options
+    };
+  }
+
+  if (options.alias) {
+    buildFnName = options.alias.build || buildFnName;
+    cloneFnName = options.alias.clone || cloneFnName;
+  }
+
+  var builderProto = {};
+  builderProto[buildFnName] = function() {
+    var params = buildParams(this._paramsForBuild, paramsDescription, options.fnName);
+    var args = [params].concat(slice(arguments));
+    return fn.apply(null, args);
   };
+  builderProto[cloneFnName] = function() {
+    var builderProto = this._builderProto;
+    var builder = Object.create(builderProto);
+    builder._paramsForBuild = clone(this._paramsForBuild);
+    builder._builderProto = builderProto;
+    return builder;
+  };
+
 
   Object.keys(paramsDescription).forEach(function(paramName) {
     var desc = paramsDescription[paramName];
-    var defaultVal = type.isArray(desc) ? desc[1] : undefined; 
+    var defaultVal = type.isArray(desc) ? desc[1] : undefined;
     var methodName;
     paramName = paramName.charAt(0) === '?' ? paramName.substr(1) : paramName;
-    
 
-    if(['build', 'clone'].indexOf(paramName) !== -1){
-      methodName = '$'+paramName;
+
+    if ([buildFnName, cloneFnName].indexOf(paramName) !== -1) {
+      methodName = '$' + paramName;
     } else {
       methodName = paramName;
     }
@@ -51,9 +67,9 @@ function makeBuilder(fn, paramsDescription, fnName) {
         this._paramsForBuild[paramName] = val;
         return this;
       }
-      
-      var paramVal = this._paramsForBuild[paramName]; 
-      return  paramVal === undefined ? defaultVal: paramVal; 
+
+      var paramVal = this._paramsForBuild[paramName];
+      return paramVal === undefined ? defaultVal : paramVal;
     };
   });
 
@@ -66,44 +82,49 @@ function makeBuilder(fn, paramsDescription, fnName) {
 
 module.exports = makeBuilder;
 
-function buildParams(paramsForBuild, descs, fnName){
+function buildParams(paramsForBuild, descs, fnName) {
   var label = fnName || 'makeBuilder';
-  return Object.keys(descs).reduce(function(result, key){
+  return Object.keys(descs).reduce(function(result, key) {
     var isRequired = key.charAt(0) !== '?';
     var paramName = !isRequired ? key.substr(1) : key;
     var builtVal = paramsForBuild[paramName];
     var descEntry = descs[key];
     var checker = type.isArray(descEntry) ? descEntry[0] : descEntry;
-    assert(type.isFunction(checker), '['+label+']param description for param "' + paramName + '": checker must be a function! given: ' + checker);
+    assert(type.isFunction(checker), '[' + label + ']param description for param "' + paramName + '": checker must be a function! given: ' + checker);
 
     var defaultVal = type.isArray(descEntry) ? descEntry[1] : undefined;
 
     var val = builtVal === undefined ? defaultVal : builtVal;
 
-    assert(isRequired ? checker(val) : (val == null || checker(val)), '['+label+']param "' + paramName + '" is invalid for build! given: ' + val);
-    
+    assert(isRequired ? checker(val) : (val == null || checker(val)), '[' + label + ']param "' + paramName + '" is invalid for build! given: ' + val);
+
     result[paramName] = val;
     return result;
   }, {});
 }
 
 
- // if(require.main === module){
- //   function print(options){
- //     console.dir(options, {depth: null});
- //   }
+// if(require.main === module){
+//   function print(options, msg){
+//     console.dir(options, {depth: null});
+//     console.log(msg);
+//   }
 
- //   var builder = makeBuilder(print, {
- //    'a': type.isNumber,
- //    'b': [type.isNumber, 4],
- //    'c': type.isString,
- //    '?d': type.isFunction,
- //    'build': type.isArray
- //   }, 'print');
+//   var builder = makeBuilder(print, {
+//    'a': type.isNumber,
+//    'b': [type.isNumber, 4],
+//    'c': type.isString,
+//    '?d': type.isFunction,
+//    'build': type.isArray
+//   }, {
+//     fnName: 'print',
+//     alias: {
+//       build: 'make'
+//     }
+//   });
 
- //   var clonedBuild = builder.a(1).c('sa').$build(['build']).clone();
- //   console.log(clonedBuild.$build());
- //   clonedBuild.b(2).build();
- //   clonedBuild.d('').build();
- // }
-
+//   var clonedBuild = builder.a(1).c('sa').build(['build']).clone();
+//   console.log(clonedBuild.build());
+//   clonedBuild.b(2).make('asdad');
+//   // clonedBuild.d('').make();
+// }
